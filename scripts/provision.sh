183322b76f4b53532a7b1a186520546ccc1cbf72
Leon <leon@nx.ie>
2023-10-28 03:37:48 +0200

    Add new workflow step

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,7 @@
 # For provisioning a fresh Vultr Alpine instance with block storage attached
 
 apk add docker docker-compose fail2ban rsync
+mkdir /root/freeflix && mkdir /root/freeflix/config && mkdir /root/freeflix/data
 rc-update add fail2ban
 rc-update add docker boot
 service docker start
