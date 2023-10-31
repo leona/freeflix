2a0ffd8d6752962aee8736999716c4915bff3313
Leon <leon@nx.ie>
2023-10-31 13:47:06 +0100

    Fix download error

--- apps/api/src/handlers/downloads.ts
+++ apps/api/src/handlers/downloads.ts
@@ -69,7 +69,7 @@ export const download = async (ctx) => {
   }
 
   const filepath = findLargestFileInPath(
-    process.env.OUTPUT_PATH + decoded.query
+    `${process.env.OUTPUT_PATH}/${decoded.query}`
   );
   const filename = filepath.split("/").pop();
   const file = Bun.file(filepath);
